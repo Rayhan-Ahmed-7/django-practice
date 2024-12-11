900c585a0256f6a64b383a51b632a91435f746c4
Rayhan-Ahmed-7 <dev.rayhan7@gmail.com>
2024-12-11 18:13:04 +0600

    :zap: task crud done

--- todo/views.py
+++ todo/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 
 # Create your views here.
@@ -6,3 +6,23 @@ def create_todo(request):
     if request.method == 'POST':
         task = request.POST['task']
         Todo.objects.create(task=task)
+        return redirect('list_todos')
+    return render(request, 'todo/create_todo.html')
+
+def list_todo(request):
+    todos = Todo.objects.all()
+    return render(request,'todo/list_todos.html',{'todos':todos})
+
+def update_todo(request,todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    if request.method == 'POST':
+        todo.task = request.POST['task']
+        todo.completed = 'completed' in request.POST
+        todo.save()
+        return redirect('list_todos')
+    return render(request,'todo/update_todo.html',{'todo':todo})
+
+def delete_todo(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    todo.delete()
+    return redirect('list_todos')
